99dce1f3b9b40ceab5f0fb1af123a6f250a2079b
nigellqx <156574394+nigellqx@users.noreply.github.com>
2024-05-31 11:29:09 +0800

    Collision Detection
    Character will stop moving when there is an object obstructing

--- Override Game/Assets/Scripts/Character.cs
+++ Override Game/Assets/Scripts/Character.cs
@@ -21,7 +21,43 @@ public class Character : MonoBehaviour
     {
         Vector2 directionVector = gameInput.GetNormalisedMovementVector();
         Vector3 newPosition = new Vector3(directionVector.x, 0f, directionVector.y);
-        transform.position += newPosition * Time.deltaTime * moveSpeed;
+
+        float characterRadius = 0.3f;
+        float characterHeight = 5.0f;
+        float distanceMoved = Time.deltaTime * moveSpeed;
+
+
+        bool willCollide = Physics.CapsuleCast(transform.position, transform.position + Vector3.up * characterHeight, characterRadius, newPosition, distanceMoved);
+
+        //For diagonal movements
+        if (willCollide) {
+
+            //Try moving in X-direction
+            Vector3 newPositionX = new Vector3(newPosition.x, 0, 0);
+            willCollide = Physics.CapsuleCast(transform.position, transform.position + Vector3.up * characterHeight, characterRadius, newPositionX, distanceMoved);
+
+            //Can move in X-direction
+            if (!willCollide) {
+                newPosition = newPositionX;
+            } else {
+
+                //Cannot move in X-direction, try moving in Z-direction
+                Vector3 newPositionZ = new Vector3(0, 0, newPosition.z);
+                willCollide = Physics.CapsuleCast(transform.position, transform.position + Vector3.up * characterHeight, characterRadius, newPositionZ, distanceMoved);
+
+                //Can move in Z-direction
+                if (!willCollide) {
+                    newPosition = newPositionZ;
+                } else {
+                    //Do nothing
+                }
+            }
+        }
+
+        if (!willCollide) { 
+            transform.position += newPosition * distanceMoved;
+        }
+
         transform.forward = Vector3.Slerp(transform.forward, newPosition, Time.deltaTime * rotateSpeed);
         isWalking = newPosition != Vector3.zero; 
     }
