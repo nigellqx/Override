a6fe9bac235d4af8f1459736c9d4b781d7aa8f35
Ong Ye Hao Darren <137365365+O-Darren@users.noreply.github.com>
2024-05-31 10:49:34 +0800

    Basic Walking Animation

--- Override Game/Assets/Scripts/Character.cs
+++ Override Game/Assets/Scripts/Character.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Character : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -12,15 +12,21 @@ public class NewBehaviourScript : MonoBehaviour
 
     [SerializeField] private float moveSpeed = 5.0f;
     [SerializeField] private float rotateSpeed = 8.0f;
-    [SerializeField] private GameInput gameInput; 
+    [SerializeField] private GameInput gameInput;
+
+    private bool isWalking;
 
     // Update is called once per frame
     void Update()
     {
         Vector2 directionVector = gameInput.GetNormalisedMovementVector();
-
         Vector3 newPosition = new Vector3(directionVector.x, 0f, directionVector.y);
         transform.position += newPosition * Time.deltaTime * moveSpeed;
         transform.forward = Vector3.Slerp(transform.forward, newPosition, Time.deltaTime * rotateSpeed);
+        isWalking = newPosition != Vector3.zero; 
+    }
+
+    public bool IsWalking() {
+        return isWalking;
     }
 }
