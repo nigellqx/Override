cb131851bed1bc895efb70be7a5c9c5f2e8d2ea6
nigellqx <156574394+nigellqx@users.noreply.github.com>
2024-06-16 16:05:23 +0800

    Parent Object
    Allowing the table to know if there is an object on the table and letting the object know which table it is on

--- Override Game/Assets/Scripts/Table.cs
+++ Override Game/Assets/Scripts/Table.cs
@@ -7,7 +7,29 @@ public class Table : MonoBehaviour
 
     [SerializeField] private pickUpObject pickUpObject;
     [SerializeField] private Transform topOfTable;
+
+    private classroomObject classroomObject;
     public void Interact() {
         Debug.Log("Interact!");
+    }
+
+    public Transform getTableTopPoint() {
+        return topOfTable;
+    }
+
+    public void setClassroomObject(classroomObject classroomObject) {
+        this.classroomObject = classroomObject;
+    }
+
+    public classroomObject GetClassroomObject() {
+        return classroomObject;
+    }
+
+    public void clearClassroomObject() {
+        this.classroomObject = null;
+    }
+
+    public bool hasClassroomObject() {
+        return this.classroomObject != null;
     }
 }
--- Override Game/Assets/Scripts/classroomObject.cs
+++ Override Game/Assets/Scripts/classroomObject.cs
@@ -6,7 +6,30 @@ public class classroomObject : MonoBehaviour {
 
     [SerializeField] private pickUpObject pickUpObject;
 
+    private Table table;
+
     public pickUpObject getPickUpObject() {
         return pickUpObject;
     } 
+
+    public void setTable(Table table) {
+        if (this.table != null) {
+            this.table.clearClassroomObject();
+        }
+
+        this.table = table;
+
+        if (this.table.hasClassroomObject()) {
+            Debug.LogError("Table already has an item");
+        }
+
+        table.setClassroomObject(this);
+
+        transform.parent = table.getTableTopPoint();
+        transform.localPosition = Vector3.zero; 
+    }
+
+    public Table getTable() {
+        return table;
+    }
 }
