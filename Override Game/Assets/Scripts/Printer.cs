e3dde2d7e6fc23cdb00c2afb9a3f4376e065cf48
Ong Ye Hao Darren <137365365+O-Darren@users.noreply.github.com>
2024-06-20 21:17:09 +0800

    Updated ComputerTable
    ComputerTable can now only take in books, and will only carry out its interact action if a book is on it

--- Override Game/Assets/Scripts/Printer.cs
+++ Override Game/Assets/Scripts/Printer.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Table : Furniture {
-   
+public class Printer : Furniture
+{
+
     public override void Interact(Character character) {
         if (!hasClassroomObject()) {
             if (character.hasClassroomObject()) {
