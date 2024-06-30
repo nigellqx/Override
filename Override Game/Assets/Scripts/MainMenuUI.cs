638f96cb99b1bf8d29e556c01897514508ff846f
Ong Ye Hao Darren <137365365+O-Darren@users.noreply.github.com>
2024-06-30 15:57:08 +0800

    Basic Controls Page
    Implemented basic controls page, that tells the player the controls for the game

--- Override Game/Assets/Scripts/MainMenuUI.cs
+++ Override Game/Assets/Scripts/MainMenuUI.cs
@@ -7,11 +7,22 @@ using UnityEngine.UI;
 public class MainMenuUI : MonoBehaviour
 {
     [SerializeField] private Button playButton;
+    [SerializeField] private Button controlsButton;
     [SerializeField] private Button quitButton;
+    [SerializeField] private Button backButton;
+    [SerializeField] private Transform instructionUI;
 
     private void Awake() {
         playButton.onClick.AddListener(() => {
             SceneManager.LoadScene("GameScene");
+        });
+
+        controlsButton.onClick.AddListener(() => {
+            instructionUI.gameObject.SetActive(true);
+        });
+
+        backButton.onClick.AddListener(() => {
+            instructionUI.gameObject.SetActive(false);
         });
 
         quitButton.onClick.AddListener(() => { 
